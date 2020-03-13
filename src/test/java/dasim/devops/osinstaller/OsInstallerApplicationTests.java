4dcd676f7d64f78c5d4fc49301cc5708189ef8a7
Damien Simon <damien.simon@rca.fr>
2020-03-13 17:04:54 +0100

    chore: initialization of the maven project

---
+++ src/test/java/dasim/devops/osinstaller/OsInstallerApplicationTests.java
@@ -0,0 +1,14 @@
+package dasim.devops.osinstaller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsInstallerApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
