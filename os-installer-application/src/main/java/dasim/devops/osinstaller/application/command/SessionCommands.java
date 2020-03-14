1c4d85eeaa7cdd883f064e3db4d304ee34b780e7
Damien Simon <damien.simon@rca.fr>
2020-03-14 18:26:37 +0100

    feat: add all desired commands to manage session

---
+++ os-installer-application/src/main/java/dasim/devops/osinstaller/application/command/SessionCommands.java
@@ -0,0 +1,56 @@
+package dasim.devops.osinstaller.application.command;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+
+/**
+ * Class for the session's shell command
+ */
+@ShellComponent
+public class SessionCommands {
+
+    /**
+     * Allow to reset the shell with no template loaded. All modifications will be lost !
+     *
+     * @return information about process
+     */
+    @ShellMethod(group = "Session commands", value = "Allow to reset the shell with no template loaded. All modifications will be lost !")
+    public String reset() {
+
+        return "no template loaded !";
+    }
+
+    /**
+     * Get all logs of your session
+     *
+     * @return information about process
+     */
+    @ShellMethod(group = "Session commands", value = "Get all logs of your session")
+    public String log() {
+
+        return "Trying to find the bullshit... ?";
+    }
+
+    /**
+     * Get history of typed commands
+     *
+     * @return information about process
+     */
+    @ShellMethod(group = "Session commands", value = "Get history of typed commands")
+    public String history() {
+
+        return "display content of file os-installer-server.log";
+    }
+
+    /**
+     * Get all logs of your session
+     *
+     * @return information about process
+     */
+    @ShellMethod(group = "Session commands", value = "Get all informations of your session")
+    public String info() {
+
+        return "{\n" + "  templateName : xxx,\n" + "  isModified : false,\n" + "  applicationsNumber : 8,\n" + "  applications : {\n" +
+                "    docker : {\n" + "      version : 1.x.y\n" + "    }\n" + "  }\n" + "}\n";
+    }
+}
