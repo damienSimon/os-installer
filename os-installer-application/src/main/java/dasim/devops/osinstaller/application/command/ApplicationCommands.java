008c92fc7cfdb57fa1872108f6789e9571b99bc4
Damien Simon <damien.simon@rca.fr>
2020-03-14 18:22:44 +0100

    feat: add all desired commands to manage applications

---
+++ os-installer-application/src/main/java/dasim/devops/osinstaller/application/command/ApplicationCommands.java
@@ -0,0 +1,60 @@
+package dasim.devops.osinstaller.application.command;
+
+import org.springframework.shell.standard.ShellComponent;
+import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+/**
+ * Class for the application's shell command
+ */
+@ShellComponent
+public class ApplicationCommands {
+
+    /**
+     * Get the complete list of applications autorized to install no matter the template
+     *
+     * @return information about process
+     */
+    @ShellMethod(key = "list --a",
+                 group = "Application commands",
+                 value = "Get the complete list of applications autorized to install no matter the template")
+    public String list() {
+
+        return "list of applications autorized !";
+    }
+
+    /**
+     * Add an application to the list of possible application to install and persist it.
+     *
+     * @param applicationName Name of the application to add (don't use space).
+     * @param filePath File path to a json file with the right content structure will be accepted
+     * @return information about process
+     */
+    @ShellMethod(key = "add --a",
+                 group = "Application commands",
+                 value = "Add an application to the list of possible application to install and persist it.")
+    public String add(
+            @ShellOption(value = {"-n", "-name"}, help = "Name of the application to add (don't use space).") String applicationName,
+            @ShellOption(value = {"-f", "-file"}, help = "File path to a json file with the right content structure will be accepted")
+                    String filePath) {
+
+        return "application " + applicationName + " added from file " + filePath + " !";
+    }
+
+    /**
+     * Add an application to the list of possible application to install and persist it.
+     *
+     * @param applicationName Name of the application to add (don't use space).
+     * @param noConfirmation Delete the application without asking a confirmation.
+     * @return information about process
+     */
+    @ShellMethod(key = "delete --a",
+                 group = "Application commands",
+                 value = "Delete an application to the list of possible application to install and persist it.")
+    public String delete(
+            @ShellOption(value = {"-n", "-name"}, help = "Name of the application to add (don't use space).") String applicationName,
+            @ShellOption(value = "-y", help = "Delete the application without asking a confirmation.") boolean noConfirmation) {
+
+        return "Are you sure to delete application " + applicationName + " ? ";
+    }
+}
