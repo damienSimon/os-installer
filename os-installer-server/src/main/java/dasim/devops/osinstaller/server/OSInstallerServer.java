5fcc366b76c44f3bc31f99adeb79cc707cce987c
Damien Simon <damien.simon@rca.fr>
2020-03-14 14:28:43 +0100

    feat: change name of the PromptProvider to "os-installer:> "

--- os-installer-server/src/main/java/dasim/devops/osinstaller/server/OSInstallerServer.java
+++ os-installer-server/src/main/java/dasim/devops/osinstaller/server/OSInstallerServer.java
@@ -1,7 +1,11 @@
 package dasim.devops.osinstaller.server;
 
+import org.jline.utils.AttributedString;
+import org.jline.utils.AttributedStyle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.shell.jline.PromptProvider;
 
 /**
  * Main class
@@ -17,6 +21,12 @@ public class OSInstallerServer {
     public static void main(String[] args) {
 
         SpringApplication.run(OSInstallerServer.class, args);
+    }
+
+    @Bean
+    public PromptProvider myPromptProvider() {
+
+        return () -> new AttributedString("os-installer:> ", AttributedStyle.DEFAULT.foreground(AttributedStyle.RED));
     }
 
 }
