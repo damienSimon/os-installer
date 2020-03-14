d5957eaa961855dea0965e35606d71732d794933
Damien Simon <damien.simon@rca.fr>
2020-03-14 14:54:10 +0100

    feat: add new commands to manage template

--- os-installer-application/src/main/java/dasim/devops/osinstaller/application/command/TemplateCommands.java
+++ os-installer-application/src/main/java/dasim/devops/osinstaller/application/command/TemplateCommands.java
@@ -16,9 +16,33 @@ public class TemplateCommands {
      * @param templateName name of the template
      * @return information about process
      */
-    @ShellMethod("load a template which contains a list of application to install")
+    @ShellMethod(group = "Template commands", value = "load a template which contains a list of application to install")
     public String load(@ShellOption String templateName) {
 
         return "template " + templateName + " loaded !";
     }
+
+    /**
+     * Allow to reset the shell with no template loaded
+     *
+     * @return information about process
+     */
+    @ShellMethod(group = "Template commands", value = "allow to reset the shell with no template loaded")
+    public String reset() {
+
+        return "no template loaded !";
+    }
+
+    /**
+     * Create a new template with the name given and no applications.
+     *
+     * @param newTemplateName name of the template
+     * @return information about process
+     */
+    @ShellMethod(group = "Template commands", value = "Create a new template with the name given and no applications.")
+    public String create(@ShellOption String newTemplateName) {
+
+        return "template " + newTemplateName + " created !";
+    }
+
 }
